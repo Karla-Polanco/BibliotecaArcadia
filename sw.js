83bdaaa99edf43fd57e1dfe7ff2a0f50aacd28dc
Karla <karlapolanco25.outlook.com>
2026-09-17 14:15:26 -0400

    feat: update theme colors to black for improved visual consistency and update service worker cache version

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * mediante caché Cache-First del App Shell y limpieza de versiones obsoletas.
  */
 
-const CACHE_NAME = 'arcadia-pwa-v117';
+const CACHE_NAME = 'arcadia-pwa-v118';
 
 // Recursos esenciales del App Shell a precachear
 const APP_SHELL_ASSETS = [
